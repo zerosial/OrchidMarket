7461f08cd05439d94086ca5e7eb9b4278de46a56
zerosial <zerosial@gmail.com>
2022-08-05 01:49:16 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,46 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`/person/${id}`);
+  };
+  const [bilionaries, setBilionaries] = useState();
+  useEffect(() => {
+    (async () => {
+      const results = await (
+        await fetch(`https://billions-api.nomadcoders.workers.dev/`)
+      ).json();
+      setBilionaries(results);
+    })();
+  }, []);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="grid items-center justify-center min-h-screen py-6 bg-slate-800">
       <Head>
-        <title>Create Next App</title>
+        <title>Billions - API</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      <main className="grid grid-cols-4 gap-12 mt-8 items-center justify-center text-center">
+        {bilionaries?.slice(0, 40).map((bill) => (
+          <div
+            className="flex flex-col justify-around items-baseline bg-slate-900 w-72 h-80 border-solid border-2 border-indigo-800 overflow-hidden"
+            key={bill.id}
+            onClick={() => onClick(bill.id)}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <img src={bill.squareImage} className="text-white" />
+            <div className="text-white text-2xl pl-2">{bill.name}</div>
+            <div className="text-white mb-12 pl-2">
+              {parseInt(bill.netWorth / 1000)}Billions / {bill.industries[0]}
+            </div>
+          </div>
+        ))}
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
