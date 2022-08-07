af041338f82dbd545eb3eaf64c1d40815d9cc0ff
zerosial <zerosial@gmail.com>
2022-08-07 18:31:59 +0900

    📰New: Skeleton UI (base tamplete)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,41 @@
-import { NextPage } from "next";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+import FloatingButton from "../components/floating-button";
+import Item from "../components/item";
+import Layout from "../components/layout";
 
-const Home: NextPage = (props) => {
-  const router = useRouter();
-  const onClick = (id) => {
-    router.push(`/person/${id}`);
-  };
-  const [bilionaries, setBilionaries] = useState();
-  useEffect(() => {
-    (async () => {
-      const results = await (
-        await fetch(`https://billions-api.nomadcoders.workers.dev/`)
-      ).json();
-      setBilionaries(results.slice(0, 40));
-    })();
-  }, []);
-
+const Home: NextPage = () => {
   return (
-    <div className="grid items-center justify-center min-h-screen py-6 bg-slate-800">
-      <Head>
-        <title>Billions - API</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="grid grid-cols-4 gap-12 mt-8 items-center justify-center text-center">
-        {bilionaries?.map((bill) => (
-          <div
-            className="flex flex-col justify-around items-baseline bg-slate-900 w-72 h-80 border-solid border-2 border-indigo-800 overflow-hidden"
-            key={bill.id}
-            onClick={() => onClick(bill.id)}
-          >
-            <img src={bill.squareImage} alt="" className="text-white" />
-            <div className="text-white text-2xl pl-2">{bill.name}</div>
-            <div className="text-white mb-12 pl-2">
-              {parseInt(bill.netWorth / 1000, 10)}Billions / {bill.industries[0]}
-            </div>
-          </div>
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            id={i}
+            key={i}
+            title="iPhone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
         ))}
-      </main>
-    </div>
+        <FloatingButton href="/items/upload">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
   );
 };
 
