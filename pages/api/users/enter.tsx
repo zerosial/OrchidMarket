479b0cfd2fac362e350d5c98e1377c720940f947
zerosial <zerosial@gmail.com>
2022-08-17 13:55:33 +0900

    v11

---
+++ pages/api/users/enter.tsx
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import sendmessage from "@libs/client/sendMessage";
+import sendEmail from "@libs/client/sendEmail";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+
+  if (phone) {
+    sendmessage(phone, payload);
+  } else if (email) {
+    sendEmail(email, payload);
+  }
+
+  return res.json({
+    ok: true,
+  });
+}
+
+export default withHandler({ methods: ["POST"], handler, isPrivate: false });
